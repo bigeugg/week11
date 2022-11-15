d9165868fd6645cf7121d1a6663518f547d543cc
Eugene Desktop <elee331@my.bcit.ca>
2022-11-15 14:54:42 -0800

    greet function

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+function greet(user, time = "day")
+{
+    console.log(`Good ${time} ${user}`)
+}
+
+greet("Eugene");
+greet("Eugene", "Night", "Morning");
+
+// function greet(user) 
+//{
+//     console.log (`Hello ${user}`); 
+//     //* this is basically console.log('Hello '+ user);
+// }
+
+// const greet = function(user) 
+//{
+//     console.log (`Hello ${user}`); 
+//     //* this is basically console.log('Hello '+ user);
+// }
+
+
+// const greet = (user) => 
+//{
+//     console.log(`Hello ${user}`);
+// }
+
+// greet();
+
