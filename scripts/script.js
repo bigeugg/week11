a5802bdfd2ead4f62d8ab40c2a3d720aac96fcfa
Eugene Desktop <elee331@my.bcit.ca>
2022-11-15 15:17:00 -0800

    circle area function

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
-function greet(user, time = "day")
-{
-    console.log(`Good ${time} ${user}`)
-}
+// function greet(user, time = "day")
+// {
+//     console.log(`Good ${time} ${user}`)
+// }
 
-greet("Eugene");
-greet("Eugene", "Night", "Morning");
+// greet("Eugene");
+// greet("Eugene", "Night", "Morning");
+
 
 // function greet(user) 
 //{
@@ -26,3 +27,12 @@ greet("Eugene", "Night", "Morning");
 
 // greet();
 
+let r = prompt("Enter a radius."); //Gives us the pop up of "Enter a radius"
+
+function circleArea (radius)
+{
+    return Math.PI * radius * radius;
+}
+
+let area = circleArea(r); //'let area' stores the values of r (which was what the user inputted)
+alert(`The area of the circle with radius ${r} is ${area}`) //alerts the output
